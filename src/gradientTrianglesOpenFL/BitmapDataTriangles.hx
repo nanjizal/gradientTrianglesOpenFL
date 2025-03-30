bfe7157bab90bbc060ff10d1c03236ba54962a61
nanjizal <nanjizal@users.noreply.github.com>
2025-03-30 15:59:28 +0100

    Update BitmapDataTriangles.hx
    add tiling and UV untested

--- src/gradientTrianglesOpenFL/BitmapDataTriangles.hx
+++ src/gradientTrianglesOpenFL/BitmapDataTriangles.hx
@@ -2,6 +2,7 @@ package gradientTrianglesOpenFL;
 import openfl.display.BitmapData;
 import gradientTrianglesOpenFL.IteratorRange;
 import lime.math.ARGB;
+import openfl.geom.Rectangle;
 @:forward
 abstract BitmapDataTriangles( BitmapData ) from BitmapData to BitmapData {
     public inline function new( bd: BitmapData ){
@@ -53,6 +54,94 @@ abstract BitmapDataTriangles( BitmapData ) from BitmapData to BitmapData {
         }
         return this;
     }
+    public inline function drawTileTriangle( ax: Float, ay: Float
+                                           , bx: Float, by: Float
+                                           , cx: Float, cy: Float, tileImage: BitmapData ): BitmapDataTriangles {
+        var s0 = ay*cx - ax*cy;
+        var sx = cy - ay;
+        var sy = ax - cx;
+        var t0 = ax*by - ay*bx;
+        var tx = ay - by;
+        var ty = bx - ax;
+        var A = -by*cx + ay*(-bx + cx) + ax*(by - cy) + bx*cy; 
+        var xIter3: IteratorRange = boundIterator3( ax, bx, cx );
+        var yIter3: IteratorRange = boundIterator3( ay, by, cy );
+        var foundY = false;
+        var s = 0.;
+        var t = 0.;
+        var sxx = 0.;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        var txx = 0.;
+        for( x in xIter3 ){
+            sxx = sx*x;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            txx = tx*x;
+            foundY = false;
+            for( y in yIter3 ){
+                s = s0 + sxx + sy*y;
+                t = t0 + txx + ty*y;
+                if( s <= 0 || t <= 0 ){
+                    // after filling break
+                    if( foundY ) break;
+                } else {
+                    if( (s + t) < A ) {
+                        // store first hit
+                        var color = tileImage.getPixel32( x % (tileImage.width), y % (tileImage.height) );
+                        this.setPixel32( x, y, color );
+                        foundY = true;
+                    } else {
+                        // after filling break
+                        if( foundY ) break;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+                    }
+                }
+            }                                                                                                                                                                                                                                                                                                                                                                                                                                
+        }
+        return this;
+    }
+
+    inline function drawUVTriangle( ax: Float, ay: Float, au: Float, av: Float
+                                  , bx: Float, by: Float, bu: Float, bv: Float
+                                  , cx: Float, cy: Float, cu: Float, cv: Float
+                                  , texture: BitmapData, win: Rectangle ): BitmapDataTriangles {
+    // switch A B as per gradient ( consider xor's )
+        var temp = au;
+        au = bu;
+        bu = temp;
+        temp = av;
+        av = bv;
+        bv = temp;
+        //
+        var bcx = bx - cx;
+        var bcy = by - cy;
+        var acx = ax - cx; 
+        var acy = ay - cy;
+        // Had to re-arrange algorithm to work so dot names may not quite make sense.
+        var dot11 = dotSame( bcx, bcy );
+        var dot12 = dot( bcx, bcy, acx, acy );
+        var dot22 = dotSame( acx, acy );
+        var denom1 = 1/( dot11 * dot22 - dot12 * dot12 );
+        var xIter3: IteratorRange = boundIterator3( ax, bx, cx );
+        var yIter3: IteratorRange = boundIterator3( ay, by, cy );
+        for( px in xIter3 ){
+            var pcx = px - cx;
+            for( py in yIter3 ){
+                var pcy = py - cy;
+                var dot31  = dot( pcx, pcy, bcx, bcy );
+                var dot32  = dot( pcx, pcy, acx, acy );
+                var ratioA = (dot22 * dot31 - dot12 * dot32) * denom1;
+                var ratioB = (dot11 * dot32 - dot12 * dot31) * denom1;
+                var ratioC = 1.0 - ratioB - ratioA;
+                if( ratioA >= 0 && ratioB >= 0 && ratioC >= 0 ){
+                    var u = au*ratioA + bu*ratioB + cu*ratioC;
+                    var v = av*ratioA + bv*ratioB + cv*ratioC;
+                    var x = Std.int( u*win.width + win.x );
+                    var y = Std.int( v*win.height + win.y );
+                    var col = texture.getPixel32( x, y );
+                    this.setPixel32( px, py, col );
+                }
+            }
+        }
+        return this;
+    }
+    
     private inline static function boundChannel( f: Float ): Int {
         var i = Std.int( f );
         if( i > 0xFF ) i = 0xFF;
